74a54a5f49b7a36f7981f685ab08491552ce3fbb
Haukur Páll Hallvarðsson <haukurpallh@gmail.com>
2014-08-09 03:23:07 +0000

    Remove registration email confirmation fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  devise :confirmable, :database_authenticatable, :recoverable, :registerable,
-    :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :registerable, :rememberable,
+    :trackable, :validatable
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
   validates_format_of :username, with: /\A[a-z0-9]{2,16}\Z/
